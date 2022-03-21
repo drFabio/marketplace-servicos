c34e0232d1fce323e08cccc064b58bbcb4fda5a1
fabio oliveira costa <fabio@heydaypeople.com>
2022-03-21 21:16:00 +0100

    adds service offer example

---
+++ pages/components/ServiceOfferForm.tsx
@@ -0,0 +1,60 @@
+import React, { FC, useState } from "react";
+/**
+ * 😀  Componente customizado de campo de formulário
+ */
+import { FormField } from "./formField";
+import styles from "./serviceOffer.module.css";
+/**
+ * 😀 Typescript, nos protege de cometer erros
+ */
+export type FormState = {
+  author: string;
+  service: string;
+  price: string;
+};
+export const ServiceOfferForm: FC = () => {
+  /**
+   * 😀 onde nossa memória atual do formulário vive
+   *
+   * Keywords: state, hooks, react
+   */
+  const [formState, setFormState] = useState<FormState>({
+    author: "",
+    service: "",
+    price: "",
+  });
+
+  return (
+    <section className={styles.container}>
+      <p>Criação de serviço</p>
+      <form action="">
+        <FormField
+          htmlId="author"
+          label="Autor"
+          value={formState.author}
+          onChange={({ target: { value } }) =>
+            setFormState((oldState) => ({ ...oldState, author: value }))
+          }
+        />
+        <FormField
+          htmlId="service"
+          label="Serviço"
+          required
+          value={formState.service}
+          onChange={({ target: { value } }) =>
+            setFormState((oldState) => ({ ...oldState, service: value }))
+          }
+        />
+        <FormField
+          htmlId="price"
+          label="Preço"
+          required
+          value={formState.price}
+          onChange={({ target: { value } }) =>
+            setFormState((oldState) => ({ ...oldState, price: value }))
+          }
+        />
+      </form>
+    </section>
+  );
+};
---
+++ pages/components/formField/index.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+
+/**
+ * 😀 Typescript e propriedades react
+ *
+ * keywords: react , props
+ */
+export type FormFieldProps = {
+  label: string;
+  htmlId: string;
+  required?: boolean;
+  type?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+export const FormField: FC<FormFieldProps> = ({
+  label,
+  htmlId,
+  type,
+  required,
+  ...rest
+}) => {
+  let parsedLabel = required ? `${label} *` : label;
+  return (
+    <label htmlFor={htmlId}>
+      {parsedLabel}
+      <input type={type} id={htmlId} required={required} {...rest} />
+    </label>
+  );
+};
